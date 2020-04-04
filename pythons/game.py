b9272335cdb548bbd774c369c5d1f3a2ba56b8ab
petia2009 <familia19842009@gmail.com>
2020-04-04 12:18:24 +0300

    second commit

--- pythons/game.py
+++ pythons/game.py
@@ -3,6 +3,9 @@ import pygame
 pygame.init()
 win = pygame.display.set_mode((500, 500))
 
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+textsurface = myfont.render('Some Text', False, (0, 0, 0))
+
 f1 = pygame.font.Font(None, 36)
 text1 = f1.render('Hello Привет', 1, (180, 0, 0))
 
