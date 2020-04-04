e7ee5fc9a382852f61caa5e88c6d26824dc952d4
petia2009 <familia19842009@gmail.com>
2020-04-04 12:22:27 +0300

    third commit

--- pythons/game.py
+++ pythons/game.py
@@ -3,12 +3,17 @@ import pygame
 pygame.init()
 win = pygame.display.set_mode((500, 500))
 
-myfont = pygame.font.SysFont('Comic Sans MS', 30)
-textsurface = myfont.render('Some Text', False, (0, 0, 0))
-
-f1 = pygame.font.Font(None, 36)
-text1 = f1.render('Hello Привет', 1, (180, 0, 0))
+myfont = pygame.font.SysFont("Comic Sans MS", 30)
+textsurface = myfont.render("Some Text", False, (0, 0, 0))
 
 run = True
 while run:
     pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    win.fill((255, 255, 255))
+    pygame.display.flip()
+
+pygame.quit()
